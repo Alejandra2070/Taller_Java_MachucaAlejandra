5f281efba1fa6a93ff83b6e23ddba3287022c893
Alejandra2070 <am3793825@gmail.com>
2025-02-11 12:56:10 -0500

    :sparkles: New homework

---
+++ ProyectoDia2/src/proyectodia2/Campers.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectodia2;
+
+/**
+ *
+ * @author Uniminuto Tibu
+ */
+public class Campers {
+    int id;
+    String fecha;
+    int usuario;
+    String contrasena;
+    String nombre;
+    String apellido;
+    String direccion;
+    String acudiente;
+    int numero_celular;
+    int numero_fijo;
+    String estado;
+    String riesgo;
+    String grupo;
+    String actividad1;
+
+    public Campers(int id, String fecha, int usuario, String contrasena, String nombre, String apellido, String direccion, String acudiente, int numero_celular, int numero_fijo, String estado, String riesgo, String grupo, String actividad1) {
+        this.id = id;
+        this.fecha = fecha;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.acudiente = acudiente;
+        this.numero_celular = numero_celular;
+        this.numero_fijo = numero_fijo;
+        this.estado = estado;
+        this.riesgo = riesgo;
+        this.grupo = grupo;
+        this.actividad1 = actividad1;
+    }
+} 
+
---
+++ ProyectoDia2/src/proyectodia2/ProyectoDia2.java
@@ -0,0 +1,12 @@
+package proyectodia2;
+
+/**
+ *
+ * @author Uniminuto Tibu
+ */
+public class ProyectoDia2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
